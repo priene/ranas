d5f13f99f90a00ef362ae448af837d75e09961d8
priene <contacto@priene.com.ar>
2015-05-20 11:12:21 -0300

    Update nosotros.php

--- application/views/frontend/nosotros.php
+++ application/views/frontend/nosotros.php
@@ -9,10 +9,10 @@
 		</div>		
 		<div class="infonos row2">
 			<div class="selloRana"></div>
-			<p>Ranas Rojas nace en el año 2011 haciendo un recorrido en sus primeras presentaciones por diferentes bares de la ciudad de La Plata, entre ellos: Hook, Pamplona, Rucache, Caetano, El Bochin y Rey Lagarto Bar.</p>
-			<p>En este ultimo trabajamos con mas de 100 bandas y artistas variados que se sumaron a la propuesta de un ciclo sostenido, desde el pasado 2012 hasta la actualidad.</p>
+			<p>Ranas Rojas nace en el a&ntilde;o 2011 haciendo un recorrido en sus primeras presentaciones por diferentes bares de la ciudad de La Plata, entre ellos: Hook, Pamplona, Rucache, Caetano, El Bochin y Rey Lagarto Bar.</p>
+			<p>En este &uacute;ltimo trabajamos con mas de 100 bandas y artistas variados que se sumaron a la propuesta de un ciclo sostenido, desde el pasado 2012 hasta la actualidad.</p>
 			<p>Entre los artistas que se sumaron a la propuesta de Ranas Rojas aparecen:</p>
-			<p>Pollera Pantalon, Orchestra Internacional du Vetex, Mansa Locura, Caracol a Contramano, Malhú Ska, Pamela Jones, Cataclismos, Ranas Toro, Josefina y yo, Sálvense quien quiera, Rufino, Maldita Blues, La tribu solar, Chancho e' monte, Peces Raros, entre otros.</p>
+			<p>Pollera Pantalon, Orchestra Internacional du Vetex, Mansa Locura, Caracol a Contramano, Malh&uacute; Ska, Pamela Jones, Cataclismos, Ranas Toro, Josefina y yo, S&aacute;lvense quien quiera, Rufino, Maldita Blues, La tribu solar, Chancho e' monte, Peces Raros, entre otros.</p>
 		</div>
 	</div>
 </div>
